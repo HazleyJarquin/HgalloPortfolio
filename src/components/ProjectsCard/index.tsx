118a2e2fa511056664778fc23aa1ce95b489e227
Hazley Jarquin <hazleyjarquin03@gmail.com>
2025-01-09 00:53:30 -0600

    refactor: update icon paths in ProjectsCard component for consistency

--- src/components/ProjectsCard/index.tsx
+++ src/components/ProjectsCard/index.tsx
@@ -9,75 +9,29 @@ interface Props {
 
 export const ProjectsCard = ({ projectData, t }: Props) => {
   const iconByTecnologyName: Record<string, React.ReactNode> = {
-    React: (
-      <img
-        src="../../../public/icons/React.svg"
-        alt="React"
-        className="w-6 h-6"
-      />
-    ),
+    React: <img src="/icons/React.svg" alt="React" className="w-6 h-6" />,
     TypeScript: (
-      <img
-        src="../../../public/icons/TypeScript.svg"
-        alt="TypeScript"
-        className="w-6 h-6"
-      />
+      <img src="/icons/Typescript.svg" alt="TypeScript" className="w-6 h-6" />
     ),
     Firebase: (
-      <img
-        src="../../../public/icons/Firebase.svg"
-        alt="Firebase"
-        className="w-6 h-6"
-      />
+      <img src="/icons/Firebase.svg" alt="Firebase" className="w-6 h-6" />
     ),
     "Material-UI": (
-      <img
-        src="../../../public/icons/MaterialUI.svg"
-        alt="Material-UI"
-        className="w-6 h-6"
-      />
+      <img src="/icons/MaterialUI.svg" alt="Material-UI" className="w-6 h-6" />
     ),
-    Axios: (
-      <img
-        src="../../../public/icons/Axios.svg"
-        alt="Axios"
-        className="w-6 h-6"
-      />
-    ),
+    Axios: <img src="/icons/Axios.svg" alt="Axios" className="w-6 h-6" />,
     Tailwind: (
-      <img
-        src="../../../public/icons/Tailwind.svg"
-        alt="Tailwind"
-        className="w-6 h-6"
-      />
+      <img src="/icons/Tailwind.svg" alt="Tailwind" className="w-6 h-6" />
     ),
     ShadcnUI: (
-      <img
-        src="../../../public/icons/Shadcn.svg"
-        alt="Tailwind"
-        className="w-6 h-6"
-      />
+      <img src="/icons/Shadcn.svg" alt="Tailwind" className="w-6 h-6" />
     ),
-    "Next.js": (
-      <img
-        src="../../../public/icons/NextJs.svg"
-        alt="NextJs"
-        className="w-6 h-6"
-      />
-    ),
+    "Next.js": <img src="/icons/NextJs.svg" alt="NextJs" className="w-6 h-6" />,
     Zustand: (
-      <img
-        src="../../../public/images/ZustandLogo.png"
-        alt="Tailwind"
-        className="w-6 h-6"
-      />
+      <img src="/images/ZustandLogo.png" alt="Tailwind" className="w-6 h-6" />
     ),
     "React-Query": (
-      <img
-        src="../../../public/icons/ReactQuery.svg"
-        alt="React-Query"
-        className="w-6 h-6"
-      />
+      <img src="/icons/ReactQuery.svg" alt="React-Query" className="w-6 h-6" />
     ),
   };
   return (
